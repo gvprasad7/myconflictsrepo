2d63b24b855eab4b5de4af162e05afb71ec71c4d
gvprasad7 <39056573+gvprasad7@users.noreply.github.com>
2020-07-23 17:24:42 +0530

    back to original for conflicts

--- feature_branch.py
+++ feature_branch.py
@@ -1,3 +1,3 @@
 print("adding new file from branch feature_bracnh")
-print("adding content second time for merging and waiting for this statement instead of add expeting edit")
+print("adding content second time for merging and waiting for this statement instead of add expeting edit; back to originial")
 
