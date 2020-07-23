516d2fef90b69f76a682ab6716cf65fbe7ed99a1
gvprasad7 <gvprasad.tav@gmail.com>
2020-07-23 12:39:48 +0530

    mac8 changes

--- new_file_mac.py
+++ new_file_mac.py
@@ -1,3 +1,2 @@
 # new line added at this line
-print("new te4xt my_new file from mac book txt")
-     
+print("new te4xt my_new file from mac book and addging chenges to the same line of code for conflicts")
