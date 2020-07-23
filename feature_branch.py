ac3f34bdbfdaff4cb80755b0e2c323c0b8809bb4
gvprasad7 <gvprasad.tav@gmail.com>
2020-07-23 17:18:51 +0530

    new content to india_master

--- feature_branch.py
+++ feature_branch.py
@@ -1,2 +1,2 @@
 print("adding new file from branch feature_bracnh")
-print("adding content second time for merging")
+print("adding content second time for merging and waiting for this statement instead of add expeting edit")
